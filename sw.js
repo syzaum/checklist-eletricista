63dfcb37ef127ca730f47fce12426cb2a0d527ed
syzaum <sy.jp157@gmail.com>
2026-08-15 00:10:16 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "checklist-eletricista-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ARQUIVOS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (nomes) {
+      return Promise.all(
+        nomes
+          .filter(function (nome) {
+            return nome !== CACHE_NAME;
+          })
+          .map(function (nome) {
+            return caches.delete(nome);
+          })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (resposta) {
+        return resposta;
+      })
+      .catch(function () {
+        return caches.match(event.request);
+      })
+  );
+});
